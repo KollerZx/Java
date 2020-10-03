c974841b2205a9e503cd68c7445ec6d2b3a2c244
Henrique Koller <38964774+KollerZx@users.noreply.github.com>
2020-10-03 15:22:55 -0300

    Create classeMath.java

---
+++ classeMath.java
@@ -0,0 +1,18 @@
+/*Classe Math
+
+java.lang.Math
+
+PI -> Math.pi
+pow ->  Exponenciação => Math.pow(<base>, <expoente>) => Math.pow(5,2) = 25
+sqrt -> Raiz quadrada => Math.sqrt(25) = 5
+cbrt -> Raiz cúbica => Math.cbrt(27) = 3
+floor -> Arredonda para baixo => Math.floor(3.9) = 3
+ceil -> Arredonda para cima => Math.ceil(4.2) = 5
+round -> Arredonda aritmeticamente => Math.round(5.6) = 6
+random -> Gerador de numeros entre 0.0 a 1.0 = > Math.random()
+          Para se ter um numero entre 5 e 10 podemos usar o seguinte artificio
+          
+          5 + Math.random() * (10-5)
+          5 + Math.random() * (9-5) -> Entre 5 e 9
+
+*/
